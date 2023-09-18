bcb475df039f97a1d8447ba266443948d6bfbb6e
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-18 19:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const info = document.getElementById("browser-info");
+let appName = navigator.appName;
+let appVersion = navigator.appVersion;
+let userAgent = navigator.userAgent;
+info.innerText = `You are using ${appName} ${appName} ${userAgent}`
